36d8e051e822b9fc41139d5e4e0a139c02d12554
Daria Koryagina <koryagina.darina@yandex.ru>
2020-05-24 02:29:40 +0300

    новые изменения

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ import popper from 'popper.js';
 import bootstrap from 'bootstrap';
 import Flickity from 'flickity'
 
-
+console.log(2)
 
 jQuery(document).ready(function($) {
     let carouselMain = document.querySelector('#slide_tel_carousel')
