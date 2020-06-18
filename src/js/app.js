461761c779aa321bd514a17aa6e506678911c5c3
Daria Koryagina <koryagina.darina@yandex.ru>
2020-06-18 22:41:00 +0300

    новые страницы проета и правки верстки

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,85 @@
-import jQuery from 'jquery';
+import * as $ from 'jquery';
 import popper from 'popper.js';
 import bootstrap from 'bootstrap';
-import Flickity from 'flickity'
+import Flickity from 'flickity-fullscreen'
+import * as select2 from 'select2'
+
 
 console.log(2)
 
-jQuery(document).ready(function($) {
-    let carouselMain = document.querySelector('#slide_tel_carousel')
+$(document).ready(function($) {
+    $('.custom__select').select2({
+        width: 'resolve'
+    });
+    $('b[role="presentation"]').hide();
+    $('.select2-selection__arrow').append('<i class="fa fa-angle-down  arrow_input"></i>');
 
+
+
+    let carouselMain = document.querySelector('.slider-main')
+    let carouselNav = document.querySelector('.slider-nav')
     let flkty = new Flickity(carouselMain, {
+        prevNextButtons: false,
         pageDots: false,
+        fullscreen: true,
+    });
+    var previousButton = document.querySelector('#start__btn');
+    previousButton.addEventListener('click', function() {
+        flkty.previous();
+    });
+
+    var nextButton = document.querySelector('#end__btn');
+    nextButton.addEventListener('click', function() {
+        flkty.next();
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    let flkty2 = new Flickity(carouselNav, {
+        asNavFor: ".slider-main",
+        contain: true,
         freeScroll: true,
-        wrapAround: true,
-        cellAlign: "left",
+        pageDots: false,
+        prevNextButtons: false
+
+
     });
 })
+
+
+let carouselRelat = document.querySelector('.carousel')
+let relat = new Flickity(carouselRelat, {
+    prevNextButtons: false,
+    pageDots: false,
+    freeScroll: true,
+    contain: true,
+    wrapAround: true
+});
+
+var startButton = document.querySelector('#btn_carusel_start');
+startButton.addEventListener('click', function() {
+    relat.previous();
+});
+
+var nextBtn = document.querySelector('#btn_carusel_next');
+nextBtn.addEventListener('click', function() {
+    relat.next();
+});
