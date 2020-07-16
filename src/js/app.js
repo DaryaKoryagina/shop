749578f4194cdcafb602e89d8a11ab22e9804be8
DaryaKoryagina <koryagina.darina@yandex.ru>
2020-07-17 02:36:31 +0300

    правки проекта

--- src/js/app.js
+++ src/js/app.js
@@ -152,4 +152,39 @@ $(document).ready(function($) {
             $("#seconds").text(seconds);
         }
     }
+
+
+
+    /* 1. Visualizing things on Hover - See next part for action on click */
+    $('#stars li').on('mouseover', function() {
+        var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
+
+        // Now highlight all the stars that's not after the current hovered star
+        $(this).parent().children('li.star').each(function(e) {
+            if (e < onStar) {
+                $(this).addClass('hover');
+            } else {
+                $(this).removeClass('hover');
+            }
+        });
+
+    }).on('mouseout', function() {
+        $(this).parent().children('li.star').each(function(e) {
+            $(this).removeClass('hover');
+        });
+    });
+
+
+    /* 2. Action to perform on click */
+    $('#stars li').on('click', function() {
+        var onStar = parseInt($(this).data('value'), 10); // The star currently selected
+        var stars = $(this).parent().children('li.star');
+        for (let i = 0; i < stars.length; i++) {
+            $(stars[i]).removeClass('selected');
+        }
+
+        for (let i = 0; i < onStar; i++) {
+            $(stars[i]).addClass('selected');
+        }
+    });
 })
